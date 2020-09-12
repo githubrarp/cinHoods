3364d12910b9231819e81280b046ce443c9dcf02
Ricardo Rodriguez <ricardoadolforodriguezperalta@gmail.com>
2020-09-12 17:44:53 -0500

    test passed

--- 06-js-function-parameters/index.js
+++ 06-js-function-parameters/index.js
@@ -5,12 +5,21 @@
  * of your name and console.log "My name is YOUR_NAME"
  */
 
+function myName(name){
+    console.log(`My name is ${name}`);
+}
+
 /**
  * Exercise 2
  *
  * create a function "myAge" which takes param(string)
  * of your age and console.log "I'm YOUR_AGE years old"
  */
+
+function myAge(age){
+    console.log(`I'm ${age} years old`);
+}
+
 
 /**
  * Exercise 3
@@ -20,9 +29,16 @@
  * "I'm from YOUR_CITY, YOUR_COUNTRY"
  */
 
+function aboutMe(city, country){
+    console.log(`I'm from ${city}, ${country}`);
+}
+
 /**
  * Exercise 4
  * create a function "logSum" which will take any 2 numbers
  * as params and log sum.
  */
 
+function logSum(a, b){
+    console.log(a + b);
+}
