526b65bc27005c85e985eccdb96033f1ebb67840
Ricardo Rodriguez <ricardoadolforodriguezperalta@gmail.com>
2020-09-28 21:26:14 -0500

    test passed

--- 28-arrow-functions-exercise/index.js
+++ 28-arrow-functions-exercise/index.js
@@ -9,12 +9,25 @@
  * and return true if they are equal or false if not
  */
 
+isEqual = (p1, p2) => {
+    if (p1 === p2) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
 /**
  * Exercise 2
  *
  * create an arrow function {isLessOrEqual0} which takes number as a param
  * and return true if it equals or less than 0 or false if not
  */
+
+isLessOrEqual0 = theNumb => {
+    return (theNumb <= 0) ? true : false;
+}
 
 /**
  * Exercise 3
@@ -27,6 +40,8 @@
  * so you will not break your function if you forget to pass params
  */
 
+timeToSeconds = (hr=0, min=0) => (min*60)+(hr*3600);
+
 /**
  * Exercise 4
  *
@@ -36,6 +51,8 @@
  * pay then return true otherwise return false
  */
 
+isGameWorthTheCandle = (prob, prize, pay) => (prob*prize > pay) ? true :  false;
+
 /**
  * Exercise 5
  *
@@ -43,3 +60,13 @@
  * as parameters and returns the string which is longest. If the strings are
  * equal in length return the string "Equal!"
  */
+
+longerString = (str1, str2) => {
+    if (str1.length === str2.length){
+        return "Equal!";
+    }else if(str1.length > str2.length){
+        return str1;
+    }else{
+        return str2;
+    }
+}
