06836a62d7dce24eb31686e4b9f89b5ac57fb615
Ricardo Rodriguez <ricardoadolforodriguezperalta@gmail.com>
2020-10-01 11:30:47 -0500

    test passed

--- 31-hof-filter-exercise/index.js
+++ 31-hof-filter-exercise/index.js
@@ -5,6 +5,16 @@
  * as a parameter and return an array but without strings
  */
 
+ noStrings = theArr =>{
+     let resultingArr = [];
+     theArr.filter(element => {
+         if(typeof(element) != 'string'){
+             resultingArr.push(element);
+         }
+     });
+     return resultingArr;
+ }
+
 /**
  * Exercise 2
  *
@@ -12,6 +22,17 @@
  * as a parameter and return an array of strings by removing
  * every other type
  */
+
+
+justStrings = anArr =>{
+        let resultingArr = [];
+        anArr.filter(element => {
+            if(typeof(element) === 'string'){
+                resultingArr.push(element);
+            }
+        });
+    return resultingArr;
+}
 
 /**
  * Exercise 3
@@ -21,6 +42,17 @@
  * that are longer than 5 characters
  */
 
+justStringsLongerThan5 = anArr =>{
+    let resultingArr = [];
+    anArr.filter(element => {
+        if(typeof(element) === 'string' && element.length > 5){
+            resultingArr.push(element);
+        }
+    });
+return resultingArr;
+}
+
+
 /**
  * Exercise 4
  *
@@ -29,12 +61,34 @@
  * that are more than 13
  */
 
+numbersMoreThan13 = anArr =>{
+    let resultingArr = [];
+    anArr.filter(element => {
+        if(typeof(element) === 'number' && element > 13){
+            resultingArr.push(element);
+        }
+    });
+return resultingArr;
+}
+
+
 /**
  * Exercise 5
  *
  * create a function {objectsOnly} that takes
  * an array as a parameter and return an array of objects
  */
+
+objectsOnly = anArr =>{
+    let resultingArr = [];
+    anArr.filter(element => {
+        if(typeof(element) === 'object'){
+            resultingArr.push(element);
+        }
+    });
+return resultingArr;
+}
+
 
 /**
  * Exercise 6
@@ -43,3 +97,21 @@
  * an array as a parameter and return an array of truthy values.
  * IMPORTANT: You still need to keep number 0.
  */
+
+const truthyOnly = anArr =>{
+/*    let resultingArr = [];
+    anArr.filter(element => {
+        if(element || element == 0){
+            console.log(typeof(element));
+            resultingArr.push(element);
+        }
+    }); */
+
+    const filteredArray = anArr.filter(item => item || item === 0);
+    console.log(filteredArray);
+    
+return filteredArray;
+}
+
+// const truthyOnly = anArr => anArr.filter(item => item || item === 0);
+
