276648fe782cd058cd9661760ef473ed0add5a4c
githubrarp <ricardoadolforodriguezperalta@gmail.com>
2020-10-15 17:47:58 -0500

    test passed and audio playing if visible

--- 42-other-listeners/index.js
+++ 42-other-listeners/index.js
@@ -6,6 +6,13 @@
  * when you double click on ".trigger"
  */
 
+const trigger = document.querySelector(".trigger");
+const popup = document.querySelector(".popup");
+
+trigger.addEventListener("dblclick", () => {
+  popup.classList.add("open");
+});
+
 /**
  * Exercise 2
  *
@@ -14,12 +21,28 @@
  * image container(".img")
  */
 
+const imageContainer = document.querySelector(".img");
+const image = document.querySelector("img");
+
+imageContainer.addEventListener("dblclick", () => {
+  image.classList.toggle("zoom");
+});
 
 /**
  * Exercise 3
  *
  * create an event listener which will stop/start music player
  * when you switch tab/come back to the tab in the browser.
- * 
+ *
  * NOTE: to test please interact with the page, and switch tabs
  */
+
+const audio = document.querySelector("audio");
+
+document.addEventListener("visibilitychange", () => {
+if (!document.hidden) {
+    audio.play();
+} else {
+    audio.pause();
+}
+});
